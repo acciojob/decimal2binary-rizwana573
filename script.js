edac125be747e08de6cc350d6164b57214fd9c5e
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-02 14:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function decimalToBinary(num) {
  Number(num).toString(2);  
 }
 
-let num = prompt('please insert decimal number');
-window.decimalToBinary = decimalToBinary(num); 
+//let num = prompt('please insert decimal number');
+window.decimalToBinary = decimalToBinary; 
